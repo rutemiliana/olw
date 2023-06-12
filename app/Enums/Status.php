ed331ad17e0602b236e3a96e758f99dd9bc8ceaf
Rute Miliana <rutemiliana13@gmail.com>
2023-06-11 22:18:03 -0300

    .

--- app/Enums/Status.php
+++ app/Enums/Status.php
@@ -2,6 +2,7 @@
 
 namespace App\Enums;
 
+// Enum é uma criação de tipagem para tipos específicos de valores. Este referenciamento é usado em $casts da model Sale
 enum status: string
 {
     CASE PEDDING = 'P';
