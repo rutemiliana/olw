c81f77b09648f15b4ceebb596a7c8f39560bbd66
Rute Miliana <rutemiliana13@gmail.com>
2023-06-11 22:00:19 -0300

    define relationship

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Client extends Model
@@ -20,4 +21,8 @@ class Client extends Model
         return $this->hasOne(Address::class);
     }
 
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
